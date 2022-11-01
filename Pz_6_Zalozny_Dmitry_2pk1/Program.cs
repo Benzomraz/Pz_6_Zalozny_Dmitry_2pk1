6718d1536bf88eb1518b9786dbdf94b1780aee59
Benzomraz <112751181+Benzomraz@users.noreply.github.com>
2022-11-01 20:45:55 +0500

    Добавьте файлы проекта.

---
+++ Pz_6_Zalozny_Dmitry_2pk1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Pz_6_Zalozny_Dmitry_2pk1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ваше число равно...");
+            int n = int.Parse(Console.ReadLine());
+            int fib = 1;
+            if(n > 0)
+            {
+                for (int i = 1; i<= n; i++)
+                {
+                    fib *= i; 
+                }
+                Console.WriteLine(fib);
+            }
+            if(n == 0)
+            {
+                Console.WriteLine(1);
+            }
+            if(n<0)
+            {
+                Console.WriteLine(-1);
+            }
+        }
+    }
+}
