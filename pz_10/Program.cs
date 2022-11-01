17b45e3d467b082f04757e4debf5d6dd17a9c72b
Benzomraz <112751181+Benzomraz@users.noreply.github.com>
2022-11-01 21:50:30 +0500

    1

---
+++ pz_10/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace pz_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text1 = Console.ReadLine();
+            text1 = System.Text.RegularExpressions.Regex.Replace(text1, @"\s+", " ");
+            text1 = text1.Trim();
+            Console.WriteLine(text1);
+        }
+    }
+}
